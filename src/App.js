44833d4403772ba9ce222fc40e824c4ab556b6c6
Srutip04 <subuhishruti75@gmail.com>
2021-12-08 00:27:20 +0530

    notes component and map function

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import logo from './logo.svg';
 import Header from './components/header/header';
 import Note from './components/note/note';
 import Footer from './components/footer/footer';
+import notes from './Notes';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Note />
+      {notes.map((noteItem) => {
+        return (
+          <Note 
+            key={noteItem.key}
+            title={noteItem.title}
+            content={noteItem.content}
+
+          />
+        )
+      })}
       <Footer />
     </div>
   );
