a1c9d59d647c7362729179519fe827ca98176d96
Srutip04 <subuhishruti75@gmail.com>
2021-12-10 00:27:33 +0530

    notes comp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
+import react, { useState } from 'react';
 import logo from './logo.svg';
 import Header from './components/header/header';
 import Note from './components/note/note';
 import Footer from './components/footer/footer';
-import notes from './Notes';
+import CreateArea from './components/create-area/createarea';
+
 import './App.css';
 
 function App() {
+  
+  const [notes, setNotes] =  useState([]);
+
+   function addNote(newNote) {
+     setNotes((prevNotes) => {
+       return [...prevNotes, newNote];
+     });
+   }
+
+   function deleteNote(id) {
+     setNotes((prevNotes) => {
+       return prevNotes.filter((noteItem, index) => {
+         return index !== id;
+       });
+     });
+   }
+
   return (
     <div className="App">
       <Header />
-      {notes.map((noteItem) => {
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem,index) => {
         return (
-          <Note 
-            key={noteItem.key}
+          <Note
+            key={index}
+            id={index}
             title={noteItem.title}
             content={noteItem.content}
-
+            onDelete={deleteNote}
           />
-        )
+        );
       })}
       <Footer />
     </div>
